46a852ca0988d18e4015f87d6ef5e7676b6924e7
Miguel Salcedo <mds796@nyu.edu>
2019-01-13 15:13:22 -0500

    Allow null orientation shift

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def serve():
     web_cam = Camera(
         camera_arguments['source'],
         camera_arguments['fps'],
-        camera_arguments['orientation'],
+        camera_arguments.get('orientation', None),
         camera_arguments['rotation'])
 
     middleware = session_middleware(SessionStorage(create_redis(arguments)))
