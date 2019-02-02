1c3807fc74da747289045b6c34e1115b804e175b
Miguel Salcedo <mds796@nyu.edu>
2019-02-02 15:31:24 -0500

    Webcam with Nginx proxy in front.

--- views/base.py
+++
@@ -1,7 +0,0 @@
-from aiohttp.web import View
-
-
-class BaseView(View):
-    def __iter__(self):
-        """See https://github.com/aio-libs/aiohttp-debugtoolbar/issues/207"""
-        return self._iter().__await__()
